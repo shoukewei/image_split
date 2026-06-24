5c1c4e6ac8f255806ce6c13208ef57208a1363b7
Shouke Wei <76237351+shoukewei@users.noreply.github.com>
2026-06-23 23:40:55 -0700

    Delete deepsim-dskit directory

--- deepsim-dskit/docs/conf.py
+++
@@ -1,6 +0,0 @@
-project = "dskit"
-author = "Shouke Wei"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
-templates_path = ["_templates"]
-exclude_patterns = ["_build"]
-html_theme = "alabaster"
